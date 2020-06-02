a2f0536f801ab1ad4c0e9d2d09a06b32e17665f3
JennyTLam <jenny.t.lam18@gmail.com>
2020-06-02 16:09:04 -0500

    check if login button renders

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -3,4 +3,9 @@ describe ('Test App', () => {
   it ('launches', () => {
     cy.visit ('/');
   });
+
+  it ('opens with sign in screen', () => {
+    cy.visit ('/');
+    cy.contains('Sign in with Google');
+  });
 });
